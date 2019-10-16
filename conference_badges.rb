55fd455eadd8a2eb4ae44c35a18b4428d2774da5
Randy Steele <steelerx155@gmail.com>
2019-10-16 16:47:11 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -14,7 +14,7 @@ end
   
 def assign_rooms(attendees)
   attendees.collect.with_index(1) do |attendee, index|
-    "Hello, #{attendee}! You'll be assigned to room #{index}."
+    "Hello, #{attendee}! You'll be assigned to room #{index}!"
   end
 end
   
