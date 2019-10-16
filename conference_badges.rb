231cae1c81594926215f0882d5cfea828d44eb18
Randy Steele <steelerx155@gmail.com>
2019-10-16 16:57:12 +0000

    Done.

--- conference_badges.rb
+++ conference_badges.rb
@@ -12,9 +12,22 @@ def batch_badge_creator(attendees)
   end
 end
   
-def assign_rooms  
-  
-  
+def assign_rooms(attendees)
+  attendees.collect.with_index(1) do |attendee, index|
+    "Hello, #{attendee}! You'll be assigned to room #{index}!"
+  end
+end
+
+def printer(attendees)
+  # binding.pry
+  batch_badge_creator(attendees).each do |badge|
+    puts badge
+end  
+
+  assign_rooms(attendees).each do |attendee|
+  puts attendee
+end  
+end  
   
   
   
