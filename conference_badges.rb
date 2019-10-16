c3032ba07fb07d242b0432d9da998e8996be6ab1
Randy Steele <steelerx155@gmail.com>
2019-10-16 15:08:14 +0000

    Done.

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,22 @@
 # Write your code here.
+require 'pry'
+
+def badge_maker(name)
+ "Hello, my name is #{name}."
+end
+
+def batch_badge_creator(attendees)
+  attendees.collect do |name| 
+    # binding.pry
+    badge_maker(name)
+  end
+end
+  
+def assign_rooms  
+  
+  
+  
+  
+  
+  
+  
