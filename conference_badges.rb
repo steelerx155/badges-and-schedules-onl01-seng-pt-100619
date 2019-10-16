e4b25053f9a0e5eb0939d6bf435f9fd0e2e57fe0
Randy Steele <steelerx155@gmail.com>
2019-10-16 16:51:42 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -19,8 +19,9 @@ def assign_rooms(attendees)
 end
 
 def printer(attendees)
-  binding.pry
-  batch_badge_creator(attendees)
+  # binding.pry
+  batch_badge_creator(attendees).each do |badge|
+    puts badge
 end  
   
   
